b66b302d53959458203b2c9e8662475d92795c67
JoelCAI <joelargentina12345@gmail.com>
2022-04-04 10:23:51 -0300

    Agregar archivos de proyecto.

---
+++ LaCadena5/Sistema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaCadena5
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            string cadena;
+
+            cadena = Validador.ValidarString("\n Ingrese una frase");
+
+            Validador.VocalTildada(cadena);
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ LaCadena5/Validador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaCadena5
+{
+    public class Validador
+    {
+        public static void VocalTildada(string cadena)
+        {
+
+            string opcion = cadena.Replace('á', 'a').Replace('é', 'e').Replace('í', 'i').Replace('ó', 'o').Replace('ú', 'u');
+
+            Console.WriteLine("\n Usted escribio " + "*" + cadena + "*" + "\n\n Ahora su frase es: " + "*" + opcion + "*");
+
+        }
+
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+        public static string ValidarString(string mensaje)
+        {
+
+            string opcion;
+            do
+            {
+
+                Console.WriteLine(mensaje);
+                opcion = Console.ReadLine();
+                if (string.IsNullOrEmpty(opcion))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" \nEl valor ingresado no puede ser vacio.");
+                    Console.WriteLine(" Por favor ingrese un valor.");
+                }
+            } while (string.IsNullOrEmpty(opcion));
+            return opcion;
+
+        }
+    }
+}
